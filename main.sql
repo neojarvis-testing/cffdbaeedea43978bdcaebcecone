2a6a519d0a381ca5db5683aee5ac4bf3acd5d409
neojarvis <techadmin@iamneo.ai>
2024-01-31 08:17:08 +0000

    Submit-project-save-commit-1/31/2024--1:47:07-PM

--- main.sql
+++ main.sql
@@ -5,8 +5,8 @@
     );
 
     -- Insert values into the table
-    INSERT INTO ExampleTable (ID, Name) VALUES (1, 'Alice');
-    INSERT INTO ExampleTable (ID, Name) VALUES (2, 'Bob');
-    INSERT INTO ExampleTable (ID, Name) VALUES (3, 'vasanth');
+    INSERT INTO ExampleTable (ID, Name) VALUES (1, 'vasanth');
+    INSERT INTO ExampleTable (ID, Name) VALUES (2, 'gopi');
+    INSERT INTO ExampleTable (ID, Name) VALUES (3, 'sagala');
 
 -- Select and view all data from the tabl
